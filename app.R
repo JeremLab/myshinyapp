ca9ef06edc6062dc152a5b4c6945cfb61cd3a5c9
Jérémie Dupont <jeremie.dupont59@icloud.com>
2025-02-07 17:52:23 +0000

    Ajout d'une table

--- app.R
+++ app.R
@@ -1,15 +1,27 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-
+library(glue)
+library(DT)
 
 ui <- fluidPage(
-  titlePanel("My First Shiny App"),
+  titlePanel("My First Shiny"),
   h1("Star Wars Characters"),
+  selectInput(
+    inputId = "choix_genre",
+    choices = c("masculine", "feminine", NA),
+    label = "Choose the good gender for the character",
+    selected = NULL,
+    multiple = FALSE,
+    selectize = TRUE,
+    width = NULL,
+    size = NULL
+  ),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("taille",
-        "Height of characters",
+      sliderInput(
+        inputId = "taille",
+        label = "Heads of characters:",
         min = 0,
         max = 250,
         value = 30
@@ -18,21 +30,41 @@ ui <- fluidPage(
     mainPanel(
       plotOutput("StarWarsPlot")
     )
-  )
+  ),
+  textOutput(outputId = "Nombre_personnes"),
+  DTOutput(outputId = "StarwarsTable")
 )
-
 
 server <- function(input, output) {
   output$StarWarsPlot <- renderPlot({
     starwars |>
       filter(height > input$taille) |>
+      filter(gender %in% input$choix_genre) |>
       ggplot(aes(x = height)) +
       geom_histogram(
         binwidth = 10,
-        fill = "darkgray",
-        collor = "with"
+        fill = "darkgrey",
+        color = "white"
+      ) + 
+      labs(
+        title = glue("Vous avez selectionné le genre : {input$choix_genre}")
       )
   })
+  
+  output$Nombre_personnes <- renderText({
+    glue("Nombre de personnages selectionnés : {
+    nrow(
+      starwars |>
+        filter(height > input$taille) |>
+        filter(gender %in% input$choix_genre))}")
+  })
+  
+  output$StarwarsTable <- renderDT({
+    starwars |>
+      filter(height > input$taille) |>
+      filter(gender %in% input$choix_genre)
+  })
 }
+
 
 shinyApp(ui = ui, server = server)
